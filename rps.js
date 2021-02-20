881eaad65b1cf286e4f30760346276cf7a61aadd
SamGarner <samuel.w.garner@gmail.com>
2021-02-19 14:45:13 -1000

    Feat: Add method to get user input.

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+// let prompt = require('prompt')
+let prompt = require('prompt-sync')();
+
 choices = ['rock', 'paper', 'scissors']
 
 let computerPlay = (choices) => {
@@ -8,3 +11,12 @@ let computerPlay = (choices) => {
 let calc_random_index = (choices) => {
   return Math.floor(choices.length * Math.random())
 }
+
+let get_user_selection = () =>  { 
+  // // prompt.start()
+  // user_choice = prompt.get("Enter your choice: 'rock', 'paper', or 'scissors':")
+  let user_choice = prompt("Enter your choice: 'rock', 'paper', or 'scissors':");
+  return user_choice
+}
+
+get_user_selection()
