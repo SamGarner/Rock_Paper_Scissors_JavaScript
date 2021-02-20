0abae7ffb8d65cb1b1b82626ab2b4640e2a78621
SamGarner <samuel.w.garner@gmail.com>
2021-02-19 14:48:10 -1000

    Refactor: Clean up get_user_selection method.

--- rps.js
+++ rps.js
@@ -12,11 +12,6 @@ let calc_random_index = (choices) => {
   return Math.floor(choices.length * Math.random())
 }
 
-let get_user_selection = () =>  { 
-  // // prompt.start()
-  // user_choice = prompt.get("Enter your choice: 'rock', 'paper', or 'scissors':")
-  let user_choice = prompt("Enter your choice: 'rock', 'paper', or 'scissors':");
-  return user_choice
-}
+let get_user_selection = () =>  prompt("Enter your choice: 'rock', 'paper', or 'scissors':");
 
 get_user_selection()
