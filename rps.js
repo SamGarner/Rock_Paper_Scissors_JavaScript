32c69ac3a61303a8ba84d529d3420fdd32bc2b4b
SamGarner <samuel.w.garner@gmail.com>
2021-03-01 19:19:21 -1000

    Feat: working best-of five game that keeps score and ignores draws.

--- rps.js
+++ rps.js
@@ -17,30 +17,36 @@ let calcRandomIndex = (choices) => {
 // update to camel case
 let getPlayerSelection = () =>  prompt("Enter your choice: 'rock', 'paper', or 'scissors':").toLowerCase();
 
-let playRound = () => {
+let playRound = (computerWins, playerWins) => {
   playerChoice = getPlayerSelection()
   computerSelection = computerPlay(choices)
-  announceRoundResult(playerChoice, computerSelection)
+  winner = getWinner(playerChoice, computerSelection)
+  announceRoundResult(playerChoice, computerSelection, winner)
+  // updateScore(winner, computerWins, playerWins)
+  return winner
 }
 
 let getWinner = (playerSelection, computerSelection) => {
-  if (playerSelection === 'rock') {
+  if (playerSelection === computerSelection) {
+    return 'draw'
+  }
+  else if (playerSelection === 'rock') {
     return computerSelection === 'scissors' ? 'player' : 'computer'
   }
   else if (playerSelection === 'scissors') {
     return computerSelection === 'paper' ? 'player' : 'computer'
   }
-  else { // player === 'paper'
+  else { // (playerSelection === 'paper')
     return computerSelection === 'rock' ? 'player' : 'computer'
   }
 }
 
-let announceRoundResult = (playerSelection, computerSelection) => {
-  if (playerSelection === computerSelection) {       // parentheses issue resolved
+let announceRoundResult = (playerSelection, computerSelection, winner) => {
+  if (playerSelection === computerSelection) {       // parentheses issue resolved  //refacotr
     announceDraw(playerSelection, computerSelection)
   }
   else {
-    announceRoundWinner(playerSelection, computerSelection);
+    announceRoundWinner(playerSelection, computerSelection, winner);
   }
 }
 
@@ -49,8 +55,8 @@ let announceDraw = (playerSelection, computerSelection) => {
   console.log("It's a draw!")
 }
 
-let announceRoundWinner = (playerSelection, computerSelection) => {
-  winner = getWinner(playerSelection, computerSelection)
+let announceRoundWinner = (playerSelection, computerSelection, winner) => {
+  // winner = getWinner(playerSelection, computerSelection)
     if (winner === 'player') {
       console.log(`You win! ${playerSelection} beats ${computerSelection}`)
     }
@@ -59,12 +65,31 @@ let announceRoundWinner = (playerSelection, computerSelection) => {
     }
 }
 
+let updateScore = (winner, computerWins, playerWins) => {
+  if (winner === 'player') {
+    playerWins += 1
+  }
+  else if (winner === 'computer') {
+    computerWins += 1
+  }
+}
+
 let game = () => {
-  let round = 1;
+  let round = 1;  // move these three variables out into global scope?
+  let playerWins = 0;
+  let computerWins = 0;
   while (round < 6) {
-    playRound();
+    winner = playRound();
+    if (winner === 'draw') {
+      continue;
+    }
+    else {
+      winner === 'player' ? playerWins += 1 : computerWins += 1;
+    }
     round += 1;
   }
+  console.log(`player wins: ${playerWins}`)
+  console.log(`computer wins: ${computerWins}`)
 }
 
 game();
